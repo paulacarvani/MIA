5ebc9712d1069060efacd7d20b5909e7a583c553
paulacarvani <3312@holbertonschool.com>
2023-05-08 14:41:18 -0500

    :tada: Add the function to read images

---
+++ server/functions/read.js
@@ -0,0 +1,26 @@
+const spawn = require("child_process").spawn;
+
+function read() {
+
+    const child = spawn('/bin/python3',
+        ['/home/holbertonpc/Documents/Holbie/mia/server/functions/python/miaread.py']);
+
+    child.stdout.on('data', (data) => {
+        console.log(`stdout:\n${data}`);
+    });
+
+    child.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+
+    child.on('error', (error) => {
+        console.error(`error: ${error.message}`);
+    });
+
+    child.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
+}
+
+
+module.exports = read
--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const read = require('./functions/read')
 
 app.get("/api", (req, res) => {
     res.json({"users": ["userOne", "userTwo", "userThree"]})
+})
+app.get("/read", (req, res) => {
+    read()
 })
 
 app.listen(5000, () => {
